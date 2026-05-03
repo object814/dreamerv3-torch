bdb6c990b595182f127e404b6368178e54b63371
Haoyu Zhou <catz0908@htc-g048.arcus-htc.arc.local>
2026-05-03 20:49:07 +0100

    Add wandb group and tags

--- ewc_training/dreamer_sequential_ewc.py
+++ ewc_training/dreamer_sequential_ewc.py
@@ -1196,6 +1196,8 @@ if __name__ == "__main__":
     parser.add_argument("--wandb-project", type=str, default="CCLB_Dreamerv3_Sequential_EWC")
     parser.add_argument("--wandb-run-name", type=str, default=None)
     parser.add_argument("--wandb-run-id", type=str, default=None, help="Existing WandB run id to resume with resume='must'.")
+    parser.add_argument("--wandb-group", type=str, default=None, help="WandB group; multiple seeds of one (method, taskset) share a group.")
+    parser.add_argument("--wandb-tags", nargs="*", default=None, help="WandB tags (space-separated).")
 
 
     # Checkpoint
